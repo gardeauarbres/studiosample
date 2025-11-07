73db7e01c498f9d51fb22c8183e16330f81d1dec
gardeauarbres <gard.eau.arbres@gmail.com>
2025-11-07 15:37:57 +0100

    Expose React globals for legacy bundles

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,21 @@
-import { createRoot } from "react-dom/client";
+import * as React from "react";
+import * as ReactDOMClient from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+if (typeof window !== "undefined") {
+  const globalObject = window as unknown as {
+    React?: typeof React;
+    ReactDOM?: typeof ReactDOMClient;
+  };
+
+  if (!globalObject.React) {
+    globalObject.React = React;
+  }
+
+  if (!globalObject.ReactDOM) {
+    globalObject.ReactDOM = ReactDOMClient;
+  }
+}
+
+ReactDOMClient.createRoot(document.getElementById("root")!).render(<App />);
